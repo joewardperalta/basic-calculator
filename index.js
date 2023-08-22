dcf42cb958bf59967a0bde36de807c3c84b5463a
Joeward Peralta <joeward_perata@outlook.com>
2023-08-21 21:07:27 -0400

    Fix Bugs

--- index.js
+++ index.js
@@ -1,118 +1,126 @@
-"use strict";
+'use strict';
 
-let numberElement = document.getElementById("number");
-let expressionElement = document.getElementById("expression");
+let numberElement = document.getElementById('number');
+let expressionElement = document.getElementById('expression');
 let result = 0;
 
 function calculate(currentElement) {
-  switch (currentElement.id) {
-    case "zero-btn":
-      if (numberElement.innerHTML != "0") numberElement.innerHTML += "0";
-      break;
+   switch (currentElement.id) {
+      case 'zero-btn':
+         if (numberElement.innerHTML != '0') numberElement.innerHTML += '0';
+         break;
 
-    case "one-btn":
-      if (numberElement.innerHTML != "0") numberElement.innerHTML += "1";
-      else numberElement.innerHTML = "1";
-      break;
+      case 'one-btn':
+         if (numberElement.innerHTML != '0') numberElement.innerHTML += '1';
+         else numberElement.innerHTML = '1';
+         break;
 
-    case "two-btn":
-      if (numberElement.innerHTML != "0") numberElement.innerHTML += "2";
-      else numberElement.innerHTML = "2";
-      break;
+      case 'two-btn':
+         if (numberElement.innerHTML != '0') numberElement.innerHTML += '2';
+         else numberElement.innerHTML = '2';
+         break;
 
-    case "three-btn":
-      if (numberElement.innerHTML != "0") numberElement.innerHTML += "3";
-      else numberElement.innerHTML = "3";
-      break;
+      case 'three-btn':
+         if (numberElement.innerHTML != '0') numberElement.innerHTML += '3';
+         else numberElement.innerHTML = '3';
+         break;
 
-    case "four-btn":
-      if (numberElement.innerHTML != "0") numberElement.innerHTML += "4";
-      else numberElement.innerHTML = "4";
-      break;
+      case 'four-btn':
+         if (numberElement.innerHTML != '0') numberElement.innerHTML += '4';
+         else numberElement.innerHTML = '4';
+         break;
 
-    case "five-btn":
-      if (numberElement.innerHTML != "0") numberElement.innerHTML += "5";
-      else numberElement.innerHTML = "5";
-      break;
+      case 'five-btn':
+         if (numberElement.innerHTML != '0') numberElement.innerHTML += '5';
+         else numberElement.innerHTML = '5';
+         break;
 
-    case "six-btn":
-      if (numberElement.innerHTML != "0") numberElement.innerHTML += "6";
-      else numberElement.innerHTML = "6";
-      break;
+      case 'six-btn':
+         if (numberElement.innerHTML != '0') numberElement.innerHTML += '6';
+         else numberElement.innerHTML = '6';
+         break;
 
-    case "seven-btn":
-      if (numberElement.innerHTML != "0") numberElement.innerHTML += "7";
-      else numberElement.innerHTML = "7";
-      break;
+      case 'seven-btn':
+         if (numberElement.innerHTML != '0') numberElement.innerHTML += '7';
+         else numberElement.innerHTML = '7';
+         break;
 
-    case "eight-btn":
-      if (numberElement.innerHTML != "0") numberElement.innerHTML += "8";
-      else numberElement.innerHTML = "8";
-      break;
+      case 'eight-btn':
+         if (numberElement.innerHTML != '0') numberElement.innerHTML += '8';
+         else numberElement.innerHTML = '8';
+         break;
 
-    case "nine-btn":
-      if (numberElement.innerHTML != "0") numberElement.innerHTML += "9";
-      else numberElement.innerHTML = "9";
-      break;
+      case 'nine-btn':
+         if (numberElement.innerHTML != '0') numberElement.innerHTML += '9';
+         else numberElement.innerHTML = '9';
+         break;
 
-    case "plus-btn":
-      expressionElement.innerHTML =
-        numberElement.innerHTML + currentElement.innerHTML;
-      numberElement.innerHTML = "0";
-      break;
+      case 'plus-btn':
+         expressionElement.innerHTML =
+            numberElement.innerHTML + currentElement.innerHTML;
+         numberElement.innerHTML = '0';
+         break;
 
-    case "minus-btn":
-      expressionElement.innerHTML =
-        numberElement.innerHTML + currentElement.innerHTML;
-      numberElement.innerHTML = "0";
-      break;
+      case 'minus-btn':
+         expressionElement.innerHTML =
+            numberElement.innerHTML + currentElement.innerHTML;
+         numberElement.innerHTML = '0';
+         break;
 
-    case "times-btn":
-      expressionElement.innerHTML =
-        numberElement.innerHTML + currentElement.innerHTML;
-      numberElement.innerHTML = "0";
-      break;
+      case 'times-btn':
+         expressionElement.innerHTML =
+            numberElement.innerHTML + currentElement.innerHTML;
+         numberElement.innerHTML = '0';
+         break;
 
-    case "divide-btn":
-      expressionElement.innerHTML =
-        numberElement.innerHTML + currentElement.innerHTML;
-      numberElement.innerHTML = "0";
-      break;
+      case 'divide-btn':
+         expressionElement.innerHTML =
+            numberElement.innerHTML + currentElement.innerHTML;
+         numberElement.innerHTML = '0';
+         break;
 
-    case "equals-btn":
-      let expression = (expressionElement.innerHTML += numberElement.innerHTML);
+      case 'equals-btn':
+         if (numberElement.innerHTML != '0') {
+            let expression = (expressionElement.innerHTML +=
+               numberElement.innerHTML);
 
-      expression = expression
-        .replaceAll("×", "*")
-        .replaceAll("÷", "/")
-        .replaceAll("−", "-");
+            expression = expression
+               .replaceAll('×', '*')
+               .replaceAll('÷', '/')
+               .replaceAll('−', '-');
 
-      try {
-        let getResult = new Function("return " + expression);
-        numberElement.innerHTML = getResult();
-      } catch (error) {
-        numberElement.innerHTML = "Invalid Expression!";
-      }
+            try {
+               let getResult = new Function('return ' + expression);
+               numberElement.innerHTML = getResult();
+               expressionElement.innerHTML += ' = ';
+            } catch (error) {
+               numberElement.innerHTML = '0';
+               expressionElement.innerHTML = '';
+            }
+         }
+         break;
 
-      expressionElement.innerHTML += " = ";
-      break;
+      case 'plusminus-btn':
+         if (numberElement.innerHTML.indexOf('-') == -1)
+            numberElement.innerHTML = '-' + numberElement.innerHTML;
+         else
+            numberElement.innerHTML = numberElement.innerHTML.replace('-', '');
+         break;
 
-    case "plusminus-btn":
-      if (numberElement.innerHTML.indexOf("-") == -1)
-        numberElement.innerHTML = "-" + numberElement.innerHTML;
-      else numberElement.innerHTML = numberElement.innerHTML.replace("-", "");
-      break;
+      case 'point-btn':
+         numberElement.innerHTML += '.';
+         break;
 
-    case "delete-btn":
-      expressionElement.innerHTML = "";
-      break;
+      case 'delete-btn':
+         expressionElement.innerHTML = '';
+         break;
 
-    case "clear-btn":
-      expressionElement.innerHTML = "";
-      numberElement.innerHTML = "0";
-      break;
+      case 'clear-btn':
+         expressionElement.innerHTML = '';
+         numberElement.innerHTML = '0';
+         break;
 
-    default:
-      console.log("Key entered not available!");
-  }
+      default:
+         console.log('Key entered not available!');
+   }
 }
